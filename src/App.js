b85a66d53b3e2f90adffe68024afb8a2496d1330
frontendprof <https://github.com/frontendprof>
2021-06-14 19:03:15 +0500

    creating different pages route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react"
+import Error from "./pages/Error"
+import Home from "./pages/Home"
+import Rooms from "./pages/Rooms"
+import SingleRoom from "./pages/SingleRoom"
+
+import {  BrowserRouter as Router,  Switch,  Route,  Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      HEllo
+    <div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms" exact component={Rooms} />
+        <Route path="/rooms/:slug" exact component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+      
     </div>
   );
 }
