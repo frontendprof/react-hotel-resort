c4639f8de481e213847a83264e35f3099a8d3654
frontendprof <https://github.com/frontendprof>
2021-06-16 17:08:33 +0500

    single room details data complete

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -30,16 +30,49 @@ export default class SingleRoom extends Component {
         }
         const {name,description,capacity, pets,breakfast, size,price,extras,images}=room;
         return (
-            <StyledHero img={images[0]}>
-                <Banner title={`${name} room`}>
-                    <Link to="/rooms" className="btn-primary">
-                        Back to Rooms 
-                    </Link>
-                </Banner>
-
+            <>
+                <StyledHero img={images[0]}>
+                    <Banner title={`${name} room`}>
+                        <Link to="/rooms" className="btn-primary">
+                            Back to Rooms 
+                        </Link>
+                    </Banner>
+                </StyledHero>
                 
+                <section className="single-room">
+                    <div className="single-room-images">
+                        {images.map((item,ind)=>{
+                            return <img key={ind} src={item} alt={name} />
+                        })}
+                    </div>
+                    <div className="single-room-info">
+                        <article className="desc">
+                            <h3>details</h3>
+                            <p>{description}</p>
+                        </article>
 
-            </StyledHero>
+                        <article className="info">
+                            <h3>info</h3>
+                            <h6>price:${price}</h6>
+                            <h6>size:${size}SQFT</h6>
+                            <h6>max capacity:{
+                                capacity>1?`${capacity} people`:`${capacity} person`}
+                            </h6>
+                            <h6>{pets?"pets allowed":"no pets allowed"}</h6>
+                            <h6>{breakfast && "free breakfast included"}</h6>
+                        </article>
+                    </div>
+                </section>
+
+                <section className="room-extras">
+                    <h6>extras</h6>
+                    <ul className="extras">
+                        {extras.map((item,ind)=>(
+                            <li key={ind}>{item}</li>
+                        ))}
+                    </ul>
+                </section>
+            </>
         )
     }
 }
