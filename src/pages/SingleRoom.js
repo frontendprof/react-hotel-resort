148ddb854e47314a6e907e0db41b21bb76eee951
frontendprof <https://github.com/frontendprof>
2021-06-16 16:40:38 +0500

    single room data placement

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,13 +1,45 @@
 
 import React, { Component } from 'react'
+import defaultImg from "../images/room-1.jpeg"
+// import Hero from "../components/Hero"
+import Banner from "../components/Banner"
+import {Link} from "react-router-dom"
+import {RoomContext} from "../context"
+import StyledHero from "../components/StyledHero"
+
 
 export default class SingleRoom extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            slug:this.props.match.params.slug,
+            defaultImg
+        }
+    }
+    static contextType=RoomContext;
     render() {
-        return (
-            <div>
-                Hello from Single Room page
-
+        const {getRoom}=this.context;
+        const room=getRoom(this.state.slug);
+        if(!room){
+            return <div className="error">
+                <h3>No such room can be found</h3>
+                <Link to="/rooms" className="btn-primary">
+                    Back to Rooms
+                </Link>
             </div>
+        }
+        const {name,description,capacity, pets,breakfast, size,price,extras,images}=room;
+        return (
+            <StyledHero img={images[0]}>
+                <Banner title={`${name} room`}>
+                    <Link to="/rooms" className="btn-primary">
+                        Back to Rooms 
+                    </Link>
+                </Banner>
+
+                
+
+            </StyledHero>
         )
     }
 }
