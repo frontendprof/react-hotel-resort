3d449b759ced0707433cafec0acf07b369411602
frontendprof <https://github.com/frontendprof>
2021-06-17 12:24:30 +0500

    withRoomConsumer Higher Order Component

--- src/context.js
+++ src/context.js
@@ -48,4 +48,12 @@ export default class RoomProvider extends Component {
 
 const RoomConsumer=RoomContext.Consumer;
 
+
+export function withRoomConsumer(Component){
+    return function ConsumerWrapper(props){
+        return <RoomConsumer>
+            {value=><Component {...props} context={value} />}
+        </RoomConsumer>
+    }
+}
 export {RoomContext,RoomProvider, RoomConsumer}
